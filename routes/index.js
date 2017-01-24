0dbd1081ce9069f613461e4123aff5b84318c8db
Rajat <rajatmig29@gmail.com>
2017-01-23 20:14:49 -0800

    first

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var log = require('../logger');
+var clientlogger = require('client-logger')(router,log);
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
